58acdd3829b06c9a290251e45971bf90a7fa409c
Niels Hansen <nielswhansen@mac.com>
2010-01-14 15:00:43 -0800

    Added Future Unit Test Files

---
+++ ARKitDemo/UnitTests.m
@@ -0,0 +1,16 @@
+//
+//  UnitTests.m
+//  ARKitDemo
+//
+//  Created by Niels W Hansen on 1/3/10.
+//  Copyright 2010 Zac White. All rights reserved.
+//
+
+#import "UnitTests.h"
+
+
+@implementation UnitTests
+- (void) testPass {
+	STAssertTrue(TRUE, @"");
+}
+@end

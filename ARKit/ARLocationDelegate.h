66664dec960293ed7b1f8fd4b6651bbcedb4319d
Niels Hansen <nielswhansen@me.com>
2011-09-11 11:14:45 -0700

    Added the ability to do touch events to UIViews on the AR View Controller

--- ARKit/ARLocationDelegate.h
+++ ARKit/ARLocationDelegate.h
@@ -3,7 +3,7 @@
 //  ARKitDemo
 //
 //  Created by Jared Crawford on 2/13/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2011. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>

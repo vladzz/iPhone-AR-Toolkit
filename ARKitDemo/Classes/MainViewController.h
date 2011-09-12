8c34e8d6709896115026f4b93ac5fc4cc4d594f8
Niels Hansen <nielswhansen@me.com>
2011-09-12 09:18:27 -0700

    New MainView Controller for launching AR from.

---
+++ ARKitDemo/Classes/MainViewController.h
@@ -0,0 +1,18 @@
+//
+//  MainViewController.h
+//  ARKitDemo
+//
+//  Created by Niels Hansen on 9/11/11.
+//  Copyright 2011 Agilite Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainViewController : UIViewController<ARLocationDelegate> {
+    
+}
+
+-(IBAction) displayAR:(id) sender;
+
+@end
